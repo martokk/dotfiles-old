36d3b5f5ef314bf0cd393d603962a269d0c0ec54
martokk <techv76@gmail.com>
2017-06-01 19:05:11 -0500

    atom packages update

--- Bin/atom_package_backup.sh
+++ Bin/atom_package_backup.sh
@@ -2,6 +2,6 @@
 
 echo "Backing up list of atom packages to ~/.atom/package-list.txt"
 
-apm list --installed --bare > ~/.atom/package-list.txt`
+apm list --installed --bare > ~/.atom/package-list.txt
 
 echo "Backup of package-list.txt complete. Use '' to install packages from backup."
