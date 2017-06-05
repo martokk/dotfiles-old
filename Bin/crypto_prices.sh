d7a617c56e4016147a5a5f72dfaae5aa1712c742
martokk <techv76@gmail.com>
2017-06-05 18:06:38 -0500

    adding ETHUSD & ETHBTC

--- Bin/crypto_prices.sh
+++ Bin/crypto_prices.sh
@@ -3,7 +3,13 @@
 btc_usd=`curl -s https://api.coinmarketcap.com/v1/ticker/bitcoin/ | jq '.[0].price_usd' | tr -d '"'`
 btc_usd=`bc -l <<< "scale=0; $btc_usd/1"`
 
+eth_usd=`curl -s https://api.coinmarketcap.com/v1/ticker/ethereum/ | jq '.[0].price_usd' | tr -d '"'`
+eth_usd=`bc -l <<< "scale=0; $eth_usd/1"`
+
+eth_btc=`curl -s https://api.coinmarketcap.com/v1/ticker/ethereum/ | jq '.[0].price_btc' | tr -d '"'`
+eth_mbtc=`bc -l <<< "scale=1; $eth_btc*1000/1"`
+
 steem_btc=`curl -s https://api.coinmarketcap.com/v1/ticker/steem/ | jq '.[0].price_btc' | tr -d '"'`
 steem_mbtc=`bc -l <<< "scale=3; $steem_btc*1000/1"`
 
-echo "BTC: $"$btc_usd"   STEEM: 0"$steem_mbtc"m฿"   
+echo "BTC: $"$btc_usd" | ETH: $"$eth_usd"     ||     ETH: "$eth_mbtc"m฿ | STEEM: 0"$steem_mbtc"m฿"
